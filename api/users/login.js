c88080d128ba4c4eeeb6443ae50eb4c42e8ce35a
emilhortlund <Emil10.hortlund@gmail.com>
2025-05-15 23:50:43 +0200

    Add more logging in login.js and login.html

--- api/users/login.js
+++ api/users/login.js
@@ -4,38 +4,51 @@ const { ensureMongoConnected } = require('../utils/db');
 const User = require('../models/User');
 
 module.exports = async (req, res) => {
+    console.log('Kör /api/users/login...');
+    console.log('Request body:', req.body);
+
     if (req.method !== 'POST') {
+        console.log('Fel metod, returnerar 405...');
         return res.status(405).json({ error: 'Method not allowed' });
     }
 
     try {
         await ensureMongoConnected(req, res, async () => {
+            console.log('MongoDB ansluten, fortsätter med inloggning...');
             const { email, password } = req.body;
 
             // Kontrollera att e-post och lösenord finns
             if (!email || !password) {
+                console.log('E-post eller lösenord saknas, returnerar 400...');
                 return res.status(400).json({ error: 'E-post och lösenord krävs' });
             }
 
             // Hitta användaren
+            console.log('Söker användare med email:', email);
             const user = await User.findOne({ email });
             if (!user) {
+                console.log('Användare hittades inte, returnerar 401...');
                 return res.status(401).json({ error: 'Fel e-post eller lösenord' });
             }
 
             // Kontrollera lösenordet
+            console.log('Jämför lösenord...');
             const isPasswordValid = await bcrypt.compare(password, user.password);
             if (!isPasswordValid) {
+                console.log('Fel lösenord, returnerar 401...');
                 return res.status(401).json({ error: 'Fel e-post eller lösenord' });
             }
 
             // Generera JWT-token
+            console.log('Genererar JWT-token...');
             const token = jwt.sign({ userId: user._id, email: user.email }, 'mysecretkey', { expiresIn: '1h' });
 
             // Uppdatera lastLogin
+            console.log('Uppdaterar lastLogin...');
             user.lastLogin = new Date();
             await user.save();
 
+            console.log('Inloggning lyckades, returnerar token:', token);
             res.json({ message: 'Inloggning lyckades', token });
         });
     } catch (error) {
